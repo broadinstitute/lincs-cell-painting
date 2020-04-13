4d37dd7b7a66ccfe7b8af3336416161c2c2b017c
gwaygenomics <gregory.way@gmail.com>
2020-04-13 15:38:00 -0400

    add notebook to extract broad IDs

---
+++ metadata/platemaps/scripts/nbconverted/0.broad-sample-metadata.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Extract all Metadata for Broad IDs Assayed with Cell Painting
+
+# In[1]:
+
+
+import os
+import pandas as pd
+
+
+# In[27]:
+
+
+all_platemaps = list()
+
+platemap_dir = "platemap"
+for platemap_file in os.listdir(platemap_dir):
+    
+    # Load platemap
+    platemap = platemap_file.strip(".txt")
+    platemap_df = pd.read_csv(os.path.join(platemap_dir, platemap_file), sep='\t')
+    
+    assert platemap == platemap_df.plate_map_name.unique()[0]
+    
+    # Process platemap
+    platemap_df = (
+        platemap_df
+        .assign(broad_id=platemap_df.broad_sample.str.extract(r"(BRD[-N][A-Z0-9]+)"))
+    )
+    
+    platemap_df = (
+        platemap_df
+        .loc[:, ["broad_sample", "broad_id", "plate_map_name", "solvent"]]
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
+    
+    all_platemaps.append(platemap_df)
+
+
+# In[32]:
+
+
+# Combine platemap info
+all_platemap_df = (
+    pd.concat(all_platemaps, axis="rows")
+    .drop_duplicates()
+)
+
+print(all_platemap_df.shape)
+all_platemap_df.head()
+
+
+# In[ ]:
+
+
+
+
