2e894963ef0237c470b722660c19e99745e447a3
Niranj <srinivasniranj@gmail.com>
2020-04-15 13:48:45 -0400

    Added deprecated broad ids

--- metadata/moa/scripts/nbconverted/2.map-broad_id.py
+++ metadata/moa/scripts/nbconverted/2.map-broad_id.py
@@ -13,16 +13,51 @@ import numpy as np
 # In[2]:
 
 
-samples_2017 = pd.read_csv('clue/repurposing_samples_20170327.txt',usecols=['broad_id','pert_iname','InChIKey'],delimiter='\t', comment='!', encoding='iso-8859-1')
+repurposing_samples_2017 = pd.read_csv('clue/repurposing_samples_20170327.txt',usecols=['broad_id','pert_iname','InChIKey'],delimiter='\t', comment='!', encoding='iso-8859-1')
 drugs_2017 = pd.read_csv('clue/repurposing_drugs_20170327.txt', usecols=['pert_iname','moa','target'], delimiter='\t', comment='!', encoding='iso-8859-1')
-samples_2018 = pd.read_csv('clue/repurposing_samples_20180907.txt', usecols=['broad_id','pert_iname','InChIKey'], delimiter='\t', comment='!', encoding='iso-8859-1')
-drugs_2018 = pd.read_csv('clue/repurposing_drugs_20180907.txt', usecols=['pert_iname','moa','target'], delimiter='\t', comment='!', encoding='iso-8859-1')
-samples_2020 = pd.read_csv('clue/repurposing_samples_20200324.txt', usecols=['broad_id','pert_iname','InChIKey'], delimiter='\t', comment='!', encoding='iso-8859-1')
+repurposing_samples_2018a = pd.read_csv('clue/repurposing_samples_20180516.txt', usecols=['broad_id','pert_iname','InChIKey','deprecated_broad_id'], delimiter='\t', comment='!', encoding='iso-8859-1')
+drugs_2018a = pd.read_csv('clue/repurposing_drugs_20180516.txt', usecols=['pert_iname','moa','target'], delimiter='\t', comment='!', encoding='iso-8859-1')
+repurposing_samples_2018b = pd.read_csv('clue/repurposing_samples_20180907.txt', usecols=['broad_id','pert_iname','InChIKey','deprecated_broad_id'], delimiter='\t', comment='!', encoding='iso-8859-1')
+drugs_2018b = pd.read_csv('clue/repurposing_drugs_20180907.txt', usecols=['pert_iname','moa','target'], delimiter='\t', comment='!', encoding='iso-8859-1')
+repurposing_samples_2020 = pd.read_csv('clue/repurposing_samples_20200324.txt', usecols=['broad_id','pert_iname','InChIKey','deprecated_broad_id'], delimiter='\t', comment='!', encoding='iso-8859-1')
 drugs_2020 = pd.read_csv('clue/repurposing_drugs_20200324.txt', usecols=['pert_iname','moa','target'], delimiter='\t', comment='!', encoding='iso-8859-1')
 
 
 # In[3]:
 
+
+repurposing_samples_2017.dropna(subset=['InChIKey'], inplace=True)
+repurposing_samples_2018a.dropna(subset=['InChIKey'], inplace=True)
+repurposing_samples_2018b.dropna(subset=['InChIKey'], inplace=True)
+repurposing_samples_2020.dropna(subset=['InChIKey'], inplace=True)
+
+# 2017 version is missing deprecated_broad_id
+repurposing_samples_2017['deprecated_broad_id'] = np.nan
+
+
+# In[4]:
+
+
+samples_2017 = repurposing_samples_2017.loc[:,['broad_id','pert_iname','InChIKey']]
+deprecated_2017 = repurposing_samples_2017.loc[:,['deprecated_broad_id','pert_iname','InChIKey']]
+samples_2018a = repurposing_samples_2018a.loc[:,['broad_id','pert_iname','InChIKey']]
+deprecated_2018a = repurposing_samples_2018a.loc[:,['deprecated_broad_id','pert_iname','InChIKey']]
+samples_2018b = repurposing_samples_2018b.loc[:,['broad_id','pert_iname','InChIKey']]
+deprecated_2018b = repurposing_samples_2018b.loc[:,['deprecated_broad_id','pert_iname','InChIKey']]
+samples_2020 = repurposing_samples_2020.loc[:,['broad_id','pert_iname','InChIKey']]
+deprecated_2020 = repurposing_samples_2020.loc[:,['deprecated_broad_id','pert_iname','InChIKey']]
+
+
+# In[5]:
+
+
+# Merge broad_id and deprecated_broad_id
+
+def merge_broad_dep_id(samples,deprecated):
+    deprecated.dropna(subset=['deprecated_broad_id'], inplace=True)
+    deprecated.rename(columns={'deprecated_broad_id':'broad_id'}, inplace=True)
+    samples = samples.append(deprecated, sort=False, ignore_index=True)
+    return samples
 
 # Maps the samples to their moa and target annotations
 
@@ -61,126 +96,79 @@ def merge_target(target):
     return out_target
 
 
-# In[4]:
+# In[6]:
+
+
+samples_2017 = merge_broad_dep_id(samples_2017, deprecated_2017)
+samples_2018a = merge_broad_dep_id(samples_2018a, deprecated_2018a)
+samples_2018b = merge_broad_dep_id(samples_2018b, deprecated_2018b)
+samples_2020 = merge_broad_dep_id(samples_2020, deprecated_2020)
+
+
+# In[7]:
 
 
 samples_2017 = target_annotation(samples_2017, drugs_2017)
-samples_2018 = target_annotation(samples_2018, drugs_2018)
+samples_2018a = target_annotation(samples_2018a, drugs_2018a)
+samples_2018b = target_annotation(samples_2018b, drugs_2018b)
 samples_2020 = target_annotation(samples_2020, drugs_2020)
 
 samples_2017.head()
 
 
-# In[5]:
+# In[8]:
 
 
 samples_2017 = id_cleanup(samples_2017, "2017")
-samples_2018 = id_cleanup(samples_2018, "2018")
+samples_2018a = id_cleanup(samples_2018a, "2018a")
+samples_2018b = id_cleanup(samples_2018b, "2018b")
 samples_2020 = id_cleanup(samples_2020, "2020")
 
 samples_2017.head()
 
 
-# In[6]:
+# In[9]:
 
 
 samples_2017 = group_by_InChIKey14(samples_2017, "2017")
-samples_2018 = group_by_InChIKey14(samples_2018, "2018")
+samples_2018a = group_by_InChIKey14(samples_2018a, "2018a")
+samples_2018b = group_by_InChIKey14(samples_2018b, "2018b")
 samples_2020 = group_by_InChIKey14(samples_2020, "2020")
 
 samples_2017.head()
 
 
-# The three dataframes are merged on InChIKey14
+# The four dataframes are merged on InChIKey14
 
-# In[7]:
+# In[10]:
 
 
-merged_df = samples_2017.merge(samples_2018, on='InChIKey14', how='outer')
+merged_df = samples_2017.merge(samples_2018a, on='InChIKey14', how='outer')
+merged_df = merged_df.merge(samples_2018b, on='InChIKey14', how='outer')
 merged_df = merged_df.merge(samples_2020, on='InChIKey14', how='outer')
 merged_df= merged_df.replace(to_replace='', value=np.nan)
 
 merged_df.head()
 
 
-# In[8]:
+# In[11]:
 
 
 merged_df.to_csv('clue/broad_id_map.csv', index=False)
 
 
-# Check if deprecated_broad_id has additional information not captured already
-
-# In[9]:
-
-
-deprecated_broad_id_2018 = pd.read_csv('clue/repurposing_samples_20180907.txt',usecols=['deprecated_broad_id','InChIKey'],delimiter='\t', comment='!', encoding='iso-8859-1')
-deprecated_broad_id_2020 = pd.read_csv('clue/repurposing_samples_20200324.txt',usecols=['deprecated_broad_id','InChIKey'],delimiter='\t', comment='!', encoding='iso-8859-1')
-
-
-# In[10]:
-
-
-# Clean up depricated_broad_id
-
-def dep_id_cleanup(df, year):
-    df.deprecated_broad_id = df.deprecated_broad_id.apply(lambda x: str(x)[:13])
-    df.InChIKey = df.InChIKey.apply(lambda x: str(x)[:14])
-    df = df.drop_duplicates(['InChIKey','deprecated_broad_id']).reset_index(drop=True)
-    df = df.rename(columns={'InChIKey':'InChIKey14',
-                            'deprecated_broad_id':'deprecated_broad_id_'+year})
-
-    return df
-
-def dep_group_by_InChIKey14(df, year):
-    df = df.groupby('InChIKey14')['deprecated_broad_id_'+year].apply(lambda x: '|'.join(np.unique(x))).reset_index()
-
-    return df
-
-
-# In[11]:
-
-
-deprecated_broad_id_2018.dropna(inplace=True)
-deprecated_broad_id_2018.reset_index(drop=True, inplace=True)
-
-deprecated_broad_id_2020.dropna(inplace=True)
-deprecated_broad_id_2020.reset_index(drop=True, inplace=True)
-
-
 # In[12]:
 
 
-deprecated_broad_id_2018 = dep_id_cleanup(deprecated_broad_id_2018, "2018")
-deprecated_broad_id_2020 = dep_id_cleanup(deprecated_broad_id_2020, "2020")
+print('Total number of rows %d' % len(merged_df))
 
+
+# The following compounds in the 2020 version have not been formatted correctly
+# as InChI is being extracted instead of InChIKey for these compounds.
 
 # In[13]:
 
 
-deprecated_broad_id_2018 = dep_group_by_InChIKey14(deprecated_broad_id_2018, "2018")
-deprecated_broad_id_2020 = dep_group_by_InChIKey14(deprecated_broad_id_2020, "2020")
+print(merged_df.loc[merged_df.InChIKey14.str.startswith('InChI')][['InChIKey14','pert_iname_2020']].to_markdown())
 
-
-# In[14]:
-
-
-unique_to_2018 = deprecated_broad_id_2018.merge(samples_2017, on='InChIKey14', how='left', indicator=True)._merge!='both'
-unique_to_2020 = deprecated_broad_id_2020.merge(samples_2018, on='InChIKey14', how='left', indicator=True)._merge!='both'
-
-
-# The following compounds are not present in the 2017 version
-
-# In[15]:
-
-
-print(deprecated_broad_id_2018[unique_to_2018].reset_index(drop=True).to_markdown())
-
-
-# The following compounds are not present in the 2018 version
-
-# In[16]:
-
-
-print(deprecated_broad_id_2020[unique_to_2020].reset_index(drop=True).to_markdown())
 
