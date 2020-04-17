cf96d2c97dd5b9f801d0960d39224e388d0d07d5
Niranj <srinivasniranj@gmail.com>
2020-04-17 13:05:53 -0400

    code reformatting using nb_black

--- metadata/moa/scripts/nbconverted/2.map-broad_id.py
+++ metadata/moa/scripts/nbconverted/2.map-broad_id.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[ ]:
+
+
+get_ipython().run_line_magic('load_ext', 'nb_black')
+
+
 # In[1]:
 
 
@@ -15,12 +21,36 @@ import os.path
 # In[2]:
 
 
-cols = ['broad_id','pert_iname','InChIKey','deprecated_broad_id']
+cols = ["broad_id", "pert_iname", "InChIKey", "deprecated_broad_id"]
 
-samples_2017 = pd.read_csv('clue/repurposing_samples_20170327.txt',usecols=cols[:-1],delimiter='\t', comment='!', encoding='iso-8859-1')
-samples_2018a = pd.read_csv('clue/repurposing_samples_20180516.txt', usecols=cols, delimiter='\t', comment='!', encoding='iso-8859-1')
-samples_2018b = pd.read_csv('clue/repurposing_samples_20180907.txt', usecols=cols, delimiter='\t', comment='!', encoding='iso-8859-1')
-samples_2020 = pd.read_csv('clue/repurposing_samples_20200324.txt', usecols=cols, delimiter='\t', comment='!', encoding='iso-8859-1')
+samples_2017 = pd.read_csv(
+    "clue/repurposing_samples_20170327.txt",
+    usecols=cols[:-1],
+    delimiter="\t",
+    comment="!",
+    encoding="iso-8859-1",
+)
+samples_2018a = pd.read_csv(
+    "clue/repurposing_samples_20180516.txt",
+    usecols=cols,
+    delimiter="\t",
+    comment="!",
+    encoding="iso-8859-1",
+)
+samples_2018b = pd.read_csv(
+    "clue/repurposing_samples_20180907.txt",
+    usecols=cols,
+    delimiter="\t",
+    comment="!",
+    encoding="iso-8859-1",
+)
+samples_2020 = pd.read_csv(
+    "clue/repurposing_samples_20200324.txt",
+    usecols=cols,
+    delimiter="\t",
+    comment="!",
+    encoding="iso-8859-1",
+)
 
 
 # In[3]:
@@ -37,46 +67,63 @@ samples_2017 = samples_2017.assign(deprecated_broad_id=np.nan)
 # Year names are appended to column names
 # For some rows InChI are extracted instead of InChIKey -> these are fixed
 
-def id_cleanup(df, version):
-    df = (
-        df.dropna(subset=['InChIKey'])
-        .reset_index(drop=True)
-    )
 
-    df.InChIKey = (
-        df.InChIKey.apply(lambda x: inchi.InchiToInchiKey(x) if (x.startswith('InChI')) else x)
-        .apply(lambda x: str(x)[:14])
-    )
-    
+def id_cleanup(df, version):
+    df = df.dropna(subset=["InChIKey"]).reset_index(drop=True)
+
+    df.InChIKey = df.InChIKey.apply(
+        lambda x: inchi.InchiToInchiKey(x) if (x.startswith("InChI")) else x
+    ).apply(lambda x: str(x)[:14])
+
     df.broad_id = df.broad_id.apply(lambda x: str(x)[:13])
 
     df = (
-        df.drop_duplicates(['InChIKey','pert_iname','broad_id','deprecated_broad_id'])
-        .rename(columns={'pert_iname':f'pert_iname_{version}',
-                            'broad_id':f'broad_id_{version}',
-                            'deprecated_broad_id':f'deprecated_broad_id_{version}',
-                            'InChIKey':'InChIKey14'})
+        df.drop_duplicates(
+            ["InChIKey", "pert_iname", "broad_id", "deprecated_broad_id"]
+        )
+        .rename(
+            columns={
+                "pert_iname": f"pert_iname_{version}",
+                "broad_id": f"broad_id_{version}",
+                "deprecated_broad_id": f"deprecated_broad_id_{version}",
+                "InChIKey": "InChIKey14",
+            }
+        )
         .reset_index(drop=True)
     )
 
     return df
 
+
 # Grouping samples using InChIKey14 while all other fields are pipe delimited
 
+
 def group_by_InChIKey14(df, version):
-    df = df.fillna('')
-    df = df.groupby('InChIKey14').agg({f'broad_id_{version}' : lambda x: '|'.join(np.unique(x)),
-                                       f'deprecated_broad_id_{version}': lambda x: create_pipe_separated_list(list(x)),
-                                       f'pert_iname_{version}': lambda x: '|'.join(np.unique(x))}).reset_index()
+    df = df.fillna("")
+    df = (
+        df.groupby("InChIKey14")
+        .agg(
+            {
+                f"broad_id_{version}": lambda x: "|".join(np.unique(x)),
+                f"deprecated_broad_id_{version}": lambda x: create_pipe_separated_list(
+                    list(x)
+                ),
+                f"pert_iname_{version}": lambda x: "|".join(np.unique(x)),
+            }
+        )
+        .reset_index()
+    )
     return df
+
 
 # This function cleans up empty deprecated_broad_ids, de-duplicates them and then separates them with pipes
 
+
 def create_pipe_separated_list(target):
-    joined_target = ('|'.join(target)).split('|')
-    while '' in joined_target:
-        joined_target.remove('')
-    out_target = '|'.join(np.unique(joined_target))
+    joined_target = ("|".join(target)).split("|")
+    while "" in joined_target:
+        joined_target.remove("")
+    out_target = "|".join(np.unique(joined_target))
     return out_target
 
 
@@ -108,10 +155,10 @@ samples_2017.head()
 
 
 merged_list = (
-    samples_2017.merge(samples_2018a, on='InChIKey14', how='outer')
-    .merge(samples_2018b, on='InChIKey14', how='outer')
-    .merge(samples_2020, on='InChIKey14', how='outer')
-    .replace(to_replace='', value=np.nan)
+    samples_2017.merge(samples_2018a, on="InChIKey14", how="outer")
+    .merge(samples_2018b, on="InChIKey14", how="outer")
+    .merge(samples_2020, on="InChIKey14", how="outer")
+    .replace(to_replace="", value=np.nan)
 )
 
 merged_list.head()
