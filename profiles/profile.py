e0d09de053eaab52135ce132d08370dd6b213454
gwaygenomics <gregory.way@gmail.com>
2020-04-28 20:11:51 +0000

    minor variable fixes

--- profiles/profile.py
+++ profiles/profile.py
@@ -6,13 +6,16 @@ import os
 import pathlib
 import pandas as pd
 from pycytominer.aggregate import AggregateProfiles
-from pycytominer.annotate import annotate
-from pycytominer.normalize import normalize
-from pycytominer.feature_select import feature_select
-from pycytominer.audit import audit
-from pycytominer.cyto_utils.output import output
+from pycytominer import (
+    aggregate,
+    annotate,
+    normalize,
+    feature_select,
+    audit,
+    cyto_utils,
+)
 
-from utils import get_args
+from profile_utils import get_args
 
 # Load Command Line Arguments
 args = get_args()
@@ -23,12 +26,14 @@ plate_name = args.plate_name
 platemap_file = args.platemap_file
 barcode_platemap_file = args.barcode_platemap_file
 moa_file = args.moa_file
-output_dir = args.output_dir
 cell_count_dir = args.cell_count_dir
+output_dir = args.output_dir
 
 # Initialize profile processing
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(cell_count_dir, exist_ok=True)
+cell_id = "A549"
+norm_method = "mad_robustize"
 compression = "gzip"
 float_format = "%.5g"
 strata = ["Image_Metadata_Plate", "Image_Metadata_Well"]
@@ -38,78 +43,100 @@ feature_select_ops = [
     "correlation_threshold",
     "blacklist",
 ]
-moa_df = pd.read_csv(moa_file, sep='\t')
-barcode_platemap_df = pd.read_csv(barcode_platemap_file)
-external_metadata_df = moa_df.merge(barcode_platemap_df)
 
-# Step 1 - Aggregate profiles
+# Define external metadata to add to annotation
+moa_df = pd.read_csv(moa_file, sep="\t")
+barcode_platemap_df = pd.read_csv(barcode_platemap_file)
+external_metadata_df = moa_df.assign(
+    Assay_Plate_Barcode=barcode_platemap_df.Assay_Plate_Barcode.values[0],
+    Plate_Map_Name=barcode_platemap_df.Plate_Map_Name.values[0],
+    Batch_Number=barcode_platemap_df.Batch_Number.values[0],
+    Batch_Date=barcode_platemap_df.Batch_Date.values[0],
+)
+
+# Aggregate profiles
 out_file = pathlib.PurePath(output_dir, f"{plate_name}.csv.gz")
 ap = AggregateProfiles(sql_file=sql_file, strata=strata)
-ap.aggregate_profiles(output_file=out_file, float_format=float_format, compression="gzip")
+ap.aggregate_profiles(
+    output_file=out_file, float_format=float_format, compression="gzip"
+)
 
-# Step 2 - Count cells
+# Count cells
 count_file = pathlib.PurePath(cell_count_dir, f"{plate_name}_cell_count.tsv")
 cell_count_df = ap.count_cells()
-cell_count_df.assign(platemap=plate_name).to_csv(count_file, sep="\t")
+cell_count_df.to_csv(count_file, sep="\t")
 
 del ap
 
-# Step 3 - Annotate profiles
+# Annotate profiles - Level 3 Data
 anno_file = pathlib.PurePath(output_dir, f"{plate_name}_augmented.csv.gz")
 anno_df = annotate(
     profiles=out_file,
     platemap=platemap_file,
-    cell_id="A549",
+    join_on=["Metadata_well_position", "Image_Metadata_Well"],
+    cell_id=cell_id,
     format_broad_cmap=True,
     perturbation_mode="chemical",
     external_metadata=external_metadata_df,
-    external_join_left=
-    external_join_right=
-    join_on=["Metadata_well_position", "Image_Metadata_Well"],
+    external_join_left=["Metadata_broad_sample"],
+    external_join_right=["Metadata_broad_sample"],
 )
 
-# Realign annotation metadata columns
-
-
-
+# Rename columns
+anno_df = anno_df.rename(
+    {"Image_Metadata_Plate": "Metadata_Plate", "Image_Metadata_Well": "Metadata_Well"},
+    axis="columns",
+)
 
 # Output annotated file
-output(df=anno_df, output_filename=anno_file, float_format=float_format, compression="gzip")
+cyto_utils.output(
+    df=anno_df, output_filename=anno_file, float_format=float_format, compression="gzip"
+)
 
-# Step 3A - Normalize Profiles (DMSO Control)
+# Normalize Profiles (DMSO Control) - Level 4A Data
 norm_dmso_file = pathlib.PurePath(output_dir, f"{plate_name}_normalized_dmso.csv.gz")
 normalize(
     profiles=anno_df,
     samples="Metadata_broad_sample == 'DMSO'",
+    method=norm_method,
     output_file=norm_dmso_file,
     float_format=float_format,
-    compression="gzip"
+    compression="gzip",
 )
 
-# Step 3B - Normalize Profiles (Whole Plate)
+# Normalize Profiles (Whole Plate) - Level 4A Data
 norm_file = pathlib.PurePath(output_dir, f"{plate_name}_normalized.csv.gz")
 normalize(
     profiles=anno_df,
     samples="all",
+    method=norm_method,
     output_file=norm_file,
     float_format=float_format,
-    compression="gzip"
+    compression=compression,
 )
 
-# Step 4A - Feature Selection (DMSO Control)
-feat_dmso_file = pathlib.PurePath(output_dir, f"{plate_name}_normalized_feature_select_dmso.csv.gz")
+# Feature Selection (DMSO Control) - Level 4B Data
+feat_dmso_file = pathlib.PurePath(
+    output_dir, f"{plate_name}_normalized_feature_select_dmso.csv.gz"
+)
 feature_select(
     profiles=norm_dmso_file,
     features="infer",
     operation=feature_select_ops,
     output_file=feat_dmso_file,
+    float_format=float_format,
+    compression=compression,
 )
 
-# Step 4B - Feature Selection (Whole Plate)
-feat_dmso_file = pathlib.PurePath(output_dir, f"{plate_name}_normalized_feature_select.csv.gz")
+# Feature Selection (Whole Plate) - Level 4B Data
+feat_file = pathlib.PurePath(
+    output_dir, f"{plate_name}_normalized_feature_select.csv.gz"
+)
 feature_select(
     profiles=norm_file,
     features="infer",
     operation=feature_select_ops,
-    output_file=feat_dmso_file,
+    output_file=feat_file,
+    float_format=float_format,
+    compression=compression,
 )
