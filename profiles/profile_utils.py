18fdfc3364028d808f3b91ae3e823fe5b2c6ad4c
gwaygenomics <gregory.way@gmail.com>
2020-04-28 15:21:24 +0000

    add profiling utility function script

---
+++ profiles/profile_utils.py
@@ -0,0 +1,18 @@
+import pathlib
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sql_file', help='the filename of the sqlite file')
+    parser.add_argument('-b', '--batch', help='string indicating the batch name')
+    parser.add_argument('-p', '--plate_name', help='string indicating the platename')
+    parser.add_argument('-f', '--platemap_file', help='path of platmap identifiers')
+    parser.add_argument('-f', '--barcode_platemap_file', help='path of plate info')
+    parser.add_argument('-m', '--moa_file', help='path of moa/target data storing')
+    parser.add_argument('-j', '--project_name', help='string for the project name')
+    parser.add_argument('-o', '--output_dir', default=pathlib.PurePath("data/profiles"),
+                        help='the directory to output the files')
+    parser.add_argument('-c', '--cell_count_dir', help='directory to save cell counts')
+    args = parser.parse_args()
+
+    return args
