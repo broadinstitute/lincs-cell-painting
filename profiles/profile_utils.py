9f617ef7a53a44b5d798a64c166bd1ebae159432
gwaygenomics <gregory.way@gmail.com>
2020-05-04 17:09:17 -0400

    remove unused command arg

--- profiles/profile_utils.py
+++ profiles/profile_utils.py
@@ -10,7 +10,6 @@ def get_args():
     parser.add_argument("-f", "--platemap_file", help="path of platmap identifiers")
     parser.add_argument("-a", "--barcode_platemap_file", help="path of plate info")
     parser.add_argument("-m", "--moa_file", help="path of moa/target data storing")
-    parser.add_argument("-j", "--project_name", help="string for the project name")
     parser.add_argument("-o", "--output_dir", help="the directory to output the files")
     parser.add_argument("-c", "--cell_count_dir", help="directory to save cell counts")
     args = parser.parse_args()
